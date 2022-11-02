bb64b4d26f7225b5a0bb5fbf190aa4bd6986e725
mujaffar09 <110367256+mujaffar09@users.noreply.github.com>
2022-11-03 00:28:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Unicafe from './components/Unicafe';
+
+
+
+function App() {
+  return (
+    <div className="App">
+       <Unicafe />
+    </div>
+  );
+}
+
+export default App;
